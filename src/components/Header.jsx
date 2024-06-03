129dce95b85ac26aebd0a47cbff6650b7d458ced
Andreea <suntandreea@protonmail.com>
2024-06-03 15:35:19 +0300

    context api

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
-import { useRef } from 'react';
+import {useContext, useRef} from 'react';
+import {CartContext} from '../store/cart-context.jsx';
 
 import CartModal from './CartModal.jsx';
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
+export default function Header() {
+  const {items} = useContext(CartContext);
+
   const modal = useRef();
 
-  const cartQuantity = cart.items.length;
+  const cartQuantity = items.length;
 
   function handleOpenCartClick() {
     modal.current.open();
@@ -26,8 +29,6 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
     <>
       <CartModal
         ref={modal}
-        cartItems={cart.items}
-        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
         title="Your Cart"
         actions={modalActions}
       />
